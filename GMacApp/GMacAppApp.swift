c168af78d1a6d98ca202a1482dec922d0f91f490
Robert Talianu <talianu.robert@gmail.com>
2024-11-16 01:13:29 +0200

    Initial Commit

---
+++ GMacApp/GMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMacAppApp.swift
+//  GMacApp
+//
+//  Created by Robert Talianu on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
